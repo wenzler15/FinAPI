72a04bea00dea8faba5659d4ea8839281f1e0fe1
DouglasWenzler <douglas.wenzler@hogarthww.com>
2022-07-14 18:36:17 -0300

    First commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const { v4: uuidv4 } = require("uuid");
+
+const app = express();
+
+app.use(express.json());
+
+const customers = [];
+/**
+ * cpf - string
+ * name - string
+ * id - uuid
+ * statement - []
+ */
+app.post("/account", (request, response) => {
+  const { cpf, name } = request.body.cpf;
+
+  const customerExists = customers.some((customer) => customer.cpf === cpf);
+
+  if (customerExists)
+    return response.status(400).json({ error: "Customer already exists" });
+
+  customers.push({
+    cpf,
+    name,
+    id: uuidv4(),
+    statement: [],
+  });
+
+  return response.status(201).send();
+});
+
+app.listen(3333);
