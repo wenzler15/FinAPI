11826444581f79dba5d0f6a4021cc5e1fc8591a0
DouglasWenzler <douglas.wenzler@hogarthww.com>
2022-07-14 18:51:18 -0300

    Listagem de saldo

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const customers = [];
  * statement - []
  */
 app.post("/account", (request, response) => {
-  const { cpf, name } = request.body.cpf;
+  const { cpf, name } = request.body;
 
   const customerExists = customers.some((customer) => customer.cpf === cpf);
 
@@ -28,6 +28,15 @@ app.post("/account", (request, response) => {
   });
 
   return response.status(201).send();
+});
+
+app.get("/statement/:cpf", (request, response) => {
+  const { cpf } = request.params;
+
+  const customer = customers.find((customer) => customer.cpf === cpf);
+
+  console.log("-----------------", customers);
+  return response.json(customer.statement);
 });
 
 app.listen(3333);
